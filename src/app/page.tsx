87e18727a1ffe2c380d6ec6955280b1e2195a6e5
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-08-28 15:37:23 -0300

    chore: Update npm dependencies and add @radix-ui/react-slot package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ src/app/products/new/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductNewPage() {
+  return (
+    <div>
+      <h1>Product New Page</h1>
+    </div>
+  );
+}
+
+export default ProductNewPage;
