1cd29e37f801056430f7f0937b7eccd6f548beee
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-09-11 12:21:40 -0300

    feat: Add Footer component to LandingPage

--- src/page/page.tsx
+++ src/page/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "@/components/component/Header";
 import AboutUs from "@/components/component/AboutUs";
+import Footer from "@/components/component/Footer";
 
 function LandingPage() {
   return (
     <div>
       <Header />
       <AboutUs />
+      <Footer />
     </div>
   );
 }
